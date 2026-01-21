ffcb4b68f13376039b6cb263a674bd819f78347f
Dusan Savic <dusan.d.savic@gamil.com>
2026-01-21 01:28:31 +0100

    Binary search in Java

---
+++ src/binarysearch/BinarySearch.java
@@ -0,0 +1,49 @@
+package binarysearch;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        
+        int array[] = new int[100];
+        int target = 33;
+        
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        
+        int index = binarySearch(array, target);
+        
+        if(index == -1){
+            System.out.println(target + " not found!");
+        }
+        else{
+            System.out.println("Element found at " + index);
+        }
+    }
+
+    private static int binarySearch(int[] array, int target) {
+        
+        int low = 0;
+        int high = array.length - 1;
+        
+        while(low <= high){
+            int middle = low + (high - low) / 2;
+            int value = array[middle];
+            
+            
+            if (value < target) {
+                low = middle + 1;
+            }
+            else if(value > target){
+                high = middle - 1;
+            }
+            else{
+                return middle;
+            }
+            
+        }
+        
+        return -1;
+    }
+    
+}
